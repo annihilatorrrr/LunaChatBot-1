73c67020d6c843361096d29fe20c17607eea31e9
Roj Serbest <rojserbest@gmail.com>
2021-05-17 12:34:16 +0300

    Include user ID in ARQ luna command

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import { Bot } from "grammy";
-import { respond } from "./luna";
+import { ARQ } from "arq-js";
 
 const bot = new Bot(process.env.TOKEN || "");
+const arq = new ARQ(process.env.API_URL || "", process.env.API_KEY || "");
 
 bot.on("message", async (ctx) => {
   const query = ctx.message?.text || ctx.message?.caption;
@@ -13,7 +14,7 @@ bot.on("message", async (ctx) => {
     query.toLocaleLowerCase().includes(ctx.me.username.toLocaleLowerCase())
   ) {
     await ctx.replyWithChatAction("typing");
-    await ctx.reply(await respond(query), {
+    await ctx.reply(await arq.luna(query, ctx.from.id), {
       reply_to_message_id: ctx.message.message_id,
       allow_sending_without_reply: true,
     });
--- src/luna.ts
+++
@@ -1,7 +0,0 @@
-import { ARQ } from "arq-js";
-
-const arq = new ARQ(process.env.API_URL || "", process.env.API_KEY || "");
-
-export const respond = async (query: string): Promise<string> => {
-  return await arq.luna(query);
-};
